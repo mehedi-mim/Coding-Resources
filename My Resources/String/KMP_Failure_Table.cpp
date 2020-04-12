74a53ef8809e7127e69d052894478dca240c4503
Mehedi Hasan Mim <mehedi.cse.16@gmail.com>
2020-04-12 13:24:16 +0600

    Update KMP_Failure_Table.cpp

--- My Resources/String/KMP_Failure_Table.cpp
+++ My Resources/String/KMP_Failure_Table.cpp
@@ -7,23 +7,29 @@ void solve(string s)
     int P[n+10]; // Failure Table
     memset(P,0,sizeof P);
     int j = 0,i = 1;
-    while(j < i && i < n){
-       if(s[j] != s[i]){
-        if(j == 0){
-            P[i] = 0;
+    while(j < i && i < n)
+    {
+        if(s[j] != s[i])
+        {
+            if(j == 0)
+            {
+                P[i] = 0;
+                i++;
+            }
+            else
+            {
+                j = P[j-1];
+            }
+        }
+        else
+        {
+            P[i] = j+1;
             i++;
+            j++;
         }
-        else{
-            j = P[j-1];
-        }
-       }
-       else{
-        P[i] = j+1;
-        i++;
-        j++;
-       }
     }
-    for(int i = 0;i < n;i++) cout << P[i] <<" ";
+    for(int i = 0; i < n; i++)
+        cout << P[i] <<" ";
     cout << endl;
     //Every index shows length of prefix(from start) and suffix(from i) are matched.
 }
