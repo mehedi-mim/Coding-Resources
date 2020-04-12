4585699f6fc5729cc0e3a8cc628643be8b851a76
Mehedi Hasan Mim <mehedi.cse.16@gmail.com>
2020-04-12 12:57:57 +0600

    Update SlidingWindow.cpp

--- My Resources/Data Structures/SlidingWindow.cpp
+++ My Resources/Data Structures/SlidingWindow.cpp
@@ -1,14 +1,17 @@
 
 vector<int> ARR;
-void sliding_window_minimum(int K) {    // Sub-array size of K
-  // pair<int, int> represents the pair (ARR[i], i)
-  deque<pair<int, int> > window;  // keep value & position
-  for (int i = 0; i < ARR.size(); i++) {
-     while (!window.empty() && window.back().first >= ARR[i])
-       window.pop_back();
-     window.push_back(make_pair(ARR[i], i));
-     while(window.front().second <= i - K)
-       window.pop_front();
-     cout << (window.front().first) << ' ';
-  }
+void sliding_window_minimum(int K)      // Sub-array size of K
+{
+    // pair<int, int> represents the pair (ARR[i], i)
+    deque<pair<int, int> > window;  // keep value & position
+    for (int i = 0; i < ARR.size(); i++)
+    {
+        while (!window.empty() && window.back().first >= ARR[i]) window.pop_back();
+
+        window.push_back(make_pair(ARR[i], i));
+
+        while(window.front().second <= i - K) window.pop_front();
+
+        cout << (window.front().first) << ' ';
+    }
 }
